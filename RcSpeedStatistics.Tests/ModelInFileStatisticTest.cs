3bdce833bb1e371afd2a683894db5d8dbb7a3bfe
Karol Nowak <karol2nw@anwokaja.pl>
2023-05-14 13:11:37 +0200

    Final

---
+++ RcSpeedStatistics.Tests/ModelInFileStatisticTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RcSpeedStatistics.Tests
+{
+    public class ModelInFileStatisticTest
+    {
+        
+        [Test]
+        public void ModelInFileStatisticComplexTest()
+        {
+            var model = new ModelInFile("model", "test");
+            model.AddSpeedValue(43);
+            model.AddSpeedValue("36");
+            model.AddSpeedValue('f');
+            
+            var result = model.GetStatistics();
+
+            Assert.AreEqual(43, result.MaxSpeedValue);
+            Assert.AreEqual(20, result.MinSpeedValue);
+            Assert.AreEqual(33, result.AverageSpeedValue);
+            Assert.AreEqual("Third speed category", result.ModelSpeedCategory);
+        }
+
+
+    }
+}
---
+++ RcSpeedStatistics.Tests/ModelInMemoryStatisticTest.cs
@@ -0,0 +1,82 @@
+namespace RcSpeedStatistics.Tests
+{
+    public class ModelInMemoryStatisticTest
+    {
+
+        [Test]
+        public void SumTest()
+        {
+            var model = new ModelInMemory("model", "testowy");            
+            model.AddSpeedValue(43);
+            model.AddSpeedValue("36");
+            model.AddSpeedValue('f');           
+
+            var result = model.GetStatistics();
+                        
+            Assert.AreEqual(99,result.sum);
+        }
+
+        [Test]
+        public void MaxValueTest()
+        {
+            var model = new ModelInMemory("model", "testowy");
+            model.AddSpeedValue(43);
+            model.AddSpeedValue("36");
+            model.AddSpeedValue('f');
+
+            var result = model.GetStatistics();
+
+            Assert.AreEqual(43, result.MaxSpeedValue);
+        }
+
+        [Test]
+        public void MinValueTest()
+        {
+            var model = new ModelInMemory("model", "testowy");
+            model.AddSpeedValue(43);
+            model.AddSpeedValue("36");
+            model.AddSpeedValue('f');
+
+            var result = model.GetStatistics();
+
+            Assert.AreEqual(20, result.MinSpeedValue);
+        }
+
+        [Test]
+        public void AverageTest()
+        {
+            var model = new ModelInMemory("model", "testowy");
+            model.AddSpeedValue(43);
+            model.AddSpeedValue("36");
+            model.AddSpeedValue('f');
+
+            var result = model.GetStatistics();
+
+            Assert.AreEqual(33, result.AverageSpeedValue);
+        }
+
+        [Test]
+        public void SpeedCategoryTest()
+        {
+            var model = new ModelInMemory("model", "testowy");
+            model.AddSpeedValue(43);
+            model.AddSpeedValue("36");
+            model.AddSpeedValue('f');
+
+            var result = model.GetStatistics();
+
+            Assert.AreEqual("Third speed category", result.ModelSpeedCategory);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
